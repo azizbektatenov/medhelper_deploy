4f73f21235e6d1d4262bf6543a8c9b82921f094e
azizbektatenov <azizbektatenov@gmail.com>
2026-04-29 21:39:27 +0500

    Fix indentation for media URL static settings

--- medhelper/urls.py
+++ medhelper/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     path("derm/", include("derm.urls")),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+
 ]
 
-if settings.DEBUG: urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
