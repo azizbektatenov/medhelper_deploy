fefdb2d30c40614a695b4ec53aff3a3205c1ade6
azizbektatenov <azizbektatenov@gmail.com>
2026-04-29 21:04:25 +0500

    Fix static URL patterns for media files

--- medhelper/urls.py
+++ medhelper/urls.py
@@ -13,8 +13,6 @@ urlpatterns = [
     path("derm/", include("derm.urls")),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
